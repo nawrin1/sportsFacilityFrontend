38b7f35b9aebe3ff411a0ffa62d747f07ef64f10
Jannatul Ferdous Nawrin <jannatulnawrin@gmail.com>
2024-08-27 18:22:50 +0600

    popular feature section done

--- src/components/Feature/Feature.tsx
+++ src/components/Feature/Feature.tsx
@@ -3,18 +3,29 @@ import img1 from '../../assets/p1.jpg';
 import img2 from '../../assets/p2.jpg';
 import img3 from '../../assets/p3.jpg';
 import img4 from '../../assets/p4.jpg';
+import { Divider } from "@mui/material";
+import './Feature.css'
 
 const Feature = () => {
     return (
         <div className="bg-black lg:h-[900px] md:h-[600px] h-[300px] relative -z-20">
             <div className='flex w-full h-[900px] relative'>
               
-            <div className='w-1/2 lg:h-[900px] md:h-[600px] h-[300px] bg-blue-400 flex justify-center items-center'>
+            <div className='w-1/2 lg:h-[900px] md:h-[600px] h-[300px] image-container  flex justify-center items-center relative '>
             <div>
             
-            <img src={img1} alt="" className="max-w-full max-h-full " />
+            <img src={img1} alt="" className="max-w-full max-h-full p-2 " />
+            <div className="overlay"></div>
 
 
+            </div>
+            <div className="absolute lg:top-[20%] lg:left-[20%] md:top-[25%] md:left-[17%] top-[20%]">
+              <h1 className="text-white lg:text-4xl md:text-2xl text-[14px] text-center font-serif font-semibold">TAKE A LOOK AT OUR <br /> POPULAR FACILITIES
+              </h1>
+
+            </div>
+            <div  className="absolute top-[60%] lg:left-[5%] md:left-[6%] md:w-[85%] w-[70%] lg:w-[90%]">
+              <p className="text-white lg:text-xl md:text-[12px] text-[10px] text-center font-serif ">"Discover the most sought-after facilities, where every game reaches new heights. Experience unmatched quality and elevate your play at the best venues around."</p>
             </div>
           </div>
 
@@ -22,50 +33,76 @@ const Feature = () => {
               <div className='w-1/2 lg:h-[900px] md:h-[600px] h-[300px]  text-white'>
                 <div className='h-full flex flex-col '>
                 <Parallax speed={5}>
-                    <div className='w-full lg:h-[300px] md:h-[200px] h-[100px] bg-blue-800 flex'>
-                      <div className="border-2 border-black flex-1 flex items-center" >
-                          <p className="lg:text-xl md:text-[14px] text-[8px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, est id, repellendus fugit similique expedita maxime illum sunt dolores, error molestias vel soluta esse deleniti adipisci aliquid. Labore, excepturi iure!</p>
+                <div className="w-full lg:h-[300px] md:h-[200px] h-[100px] flex">
+  <div className="flex-1 flex flex-col justify-center items-end p-2">
+    <h1 className="lg:text-2xl font-semibold md:text-[16px] text-[10px] mb-1 font-serif lg:mb-2 lg:pb-2 ">
+      Gymnasium
+    </h1>
+    <p className="lg:text-[15px] md:text-[12px] text-[8px] font-serif text-right">
+      Our gymnasium is equipped with the latest technology and is perfect for
+      athletes of all levels.
+    </p>
+  </div>
+  <div className="flex-1 flex items-center rounded-xl">
+    <img src={img2} alt="Gymnasium" className="max-w-full max-h-full p-2 rounded-xl" />
+  </div>
+</div>
 
+                    <Divider sx={{ 
+                borderColor: "white", 
+                "&::before, &::after": {
+                  borderColor: "white",
+                },
+                color: "white",
+                fontSize:'10px'
+              }} variant="middle">SPORTEASE</Divider>
 
-                      </div>
-                      <div className="border-2 border-black flex-1 flex items-center ">
-                        <div>
-                        <img src={img2} alt="" className="max-w-full max-h-full " />
-                        </div>
-
-
-                      </div>
-                      
-                    </div>
                   </Parallax>
+                             
                   
                   <Parallax speed={10}>
-                    <div className='w-full lg:h-[300px] md:h-[200px] h-[100px] bg-red-800 flex'>
-                      <div className="border-2 border-black flex-1 flex items-center" >
-                          <p className="lg:text-xl md:text-[14px] text-[8px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, est id, repellendus fugit similique expedita maxime illum sunt dolores, error molestias vel soluta esse deleniti adipisci aliquid. Labore, excepturi iure!</p>
-
-
-                      </div>
-                      <div className="border-2 border-black flex-1 flex items-center ">
-                        <div>
-                        <img src={img3} alt="" className="max-w-full max-h-full " />
+                    <div className='w-full lg:h-[300px] md:h-[200px] h-[100px]  flex'>
+                    <div className="flex-1 flex flex-col justify-center items-end p-2">
+    <h1 className="lg:text-2xl font-semibold md:text-[16px] text-[10px] mb-1 font-serif lg:mb-2 lg:pb-2 ">
+    Sports Courts
+    </h1>
+    <p className="lg:text-[15px] md:text-[12px] text-[8px] font-serif text-right">
+    Our versatile courts are tailored for a range of sports, including basketball, tennis, and volleyball for competitive play.
+    </p>
+  </div>
+                      <div className="  flex-1 flex items-center ">
+                        <div className="rounded-xl">
+                        <img src={img3} alt="" className="max-w-full max-h-full p-2 rounded-xl" />
                         </div>
 
 
                       </div>
                       
                     </div>
+                    <Divider sx={{ 
+                borderColor: "white", 
+                "&::before, &::after": {
+                  borderColor: "white",
+                },
+                color: "white",
+                fontSize:'10px'
+              }} variant="middle">SPORTEASE</Divider>
                   </Parallax>
+
+                  
                   <Parallax speed={15}>
-                    <div className='w-full lg:h-[300px] md:h-[200px] h-[100px] bg-yellow-200 flex'>
-                      <div className="border-2 border-black flex-1 flex items-center" >
-                          <p className="lg:text-xl md:text-[14px] text-[8px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, est id, repellendus fugit similique expedita maxime illum sunt dolores, error molestias vel soluta esse deleniti adipisci aliquid. Labore, excepturi iure!</p>
-
-
-                      </div>
-                      <div className="border-2 border-black flex-1 flex items-center ">
-                        <div>
-                        <img src={img4} alt="" className="max-w-full max-h-full " />
+                    <div className='w-full lg:h-[300px] md:h-[200px] h-[100px]  flex'>
+                    <div className="flex-1 flex flex-col justify-center items-end p-2">
+    <h1 className="lg:text-2xl font-semibold md:text-[16px] text-[10px] mb-1 font-serif lg:mb-2 lg:pb-2 ">
+    Coaching Programs
+    </h1>
+    <p className="lg:text-[15px] md:text-[12px] text-[8px] font-serif text-right">
+    Our expert trainers provide tailored workout and training plans designed to meet your specific goals, ensuring you achieve peak performance.
+    </p>
+  </div>
+                      <div className="  flex-1 flex items-center  ">
+                        <div className="rounded-xl">
+                        <img src={img4} alt="" className="max-w-full max-h-full p-2 rounded-xl " />
                         </div>
 
 
